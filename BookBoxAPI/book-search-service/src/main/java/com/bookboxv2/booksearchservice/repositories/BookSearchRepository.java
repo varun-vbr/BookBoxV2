4bb2225b5b2b6f1ad57aa608928f7b1fe7b4958b
Varun Rao <varun.vbr@gmail.com>
2021-11-23 20:49:43 +0530

    Delete BookSearchRepository.java

--- BookBoxAPI/book-search-service/src/main/java/com/bookboxv2/booksearchservice/repositories/BookSearchRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bookboxv2.booksearchservice.repositories;
-
-import com.bookboxv2.booksearchservice.models.Book;
-import org.springframework.data.repository.query.QueryByExampleExecutor;
-
-public interface BookSearchRepository extends QueryByExampleExecutor<Book> {
-}
